d0da62ab5d490192c8557fd8eeb28cf5a3e4d3b8
ADIL PASHA <146114560+adil-pasha@users.noreply.github.com>
2025-07-06 12:22:41 +0530

    app.py

--- gigfinder/app.py
+++ gigfinder/app.py
@@ -3,7 +3,7 @@ import requests
 
 # Function to generate project listings using Hugging Face model
 def generate_jobs(skill, hf_token):
-    API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
+   API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
     headers = {
         "Authorization": f"Bearer {hf_token}"
     }
